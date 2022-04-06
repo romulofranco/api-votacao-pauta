85e839c578201f0b65a8fa9297da80e77a81d95b
Rômulo Franco <romulojosefranco@gmail.com>
2022-04-06 20:42:13 -0300

    Settings db

--- src/main/java/br/romulofranco/votacao/config/SwaggerConfig.java
+++ src/main/java/br/romulofranco/votacao/config/SwaggerConfig.java
@@ -17,7 +17,7 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
 		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-//		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
 
 	@Bean
